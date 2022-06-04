51183a474d40da69ec58171b880c4ccc817b1acc
Aftanza <zazaprabowo@gmail.com>
2022-06-05 02:29:30 +0800

    [Footer] Aftanza add footer

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -1,5 +1,35 @@
 import React from "react";
+import "./index.css"
+import pintuLogo from "./img/Pintu Logo.png"
+import linkedinLogo from "./img/LinkedIn logo.png"
+import instagramLogo from "./img/Instagram Logo.png"
 
 export default function Footer(props) {
-  return <div>Footer</div>;
+    const handleClickPintuLogo = () => {
+        
+    }
+
+    return (
+      <div className="footer">
+          <div className="flex-container">
+              <div className="box copyright">
+                  <p>Copyright © 2021 PINTU & GTD. All Rights Reserved.</p>
+              </div>
+              <div className="box image-links">
+                  <a href="https://www.google.com/">
+                      <img src={pintuLogo} className="logo pintu-logo" alt="PINTU Logo. Image failed to load." onClick={handleClickPintuLogo}/>
+                  </a>
+                  <a href="https://sg.linkedin.com/in/pintusg">
+                      <img src={linkedinLogo} className="logo linkedin-logo" alt="Linkedin Logo. Image failed to load."/>
+                  </a>
+                  <a href="https://www.instagram.com/pintugram/?hl=en">
+                      <img src={instagramLogo} className="logo instagram-logo" alt="Instagram Logo. Image failed to load."/>
+                  </a>
+              </div>
+              <div className="box special-thanks">
+                <p>Special thanks to the developer team: GTD IT</p>
+              </div>
+          </div>
+      </div>
+    );
 }
