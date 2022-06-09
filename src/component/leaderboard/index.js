da9d722d69ac6d190ebe48176b2c89c93b35ab92
joanne-ch <JOANNECH001@e.ntu.edu.sg>
2022-06-09 14:16:50 +0800

    [Leaderboard] - Change file location from lbmanager to leaderboard

--- src/component/leaderboard/index.js
+++ src/component/leaderboard/index.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import "./leaderboard.css";
+import Title from './Group.png';
+import axios from "axios";
+
 
 export default function Leaderboard(props) {
-  return <div>Leaderboard</div>;
+  const [data, setData] = React.useState([]);
+  const getData = async() => {
+    const res = await fetch("http://gtd-xxiv-website-backend.herokuapp.com/")
+    const og_data = await res.json()
+    setData(og_data) 
+  }
+
+  React.useEffect(() => {getData()}, []);
+
+  return (
+  <div className="LB_manager">
+
+    <img src={Title} className="title"/>
+
+    <div className="leaderboard_parent">
+        <table className="leaderboard_child">
+          <tr>
+             <th className="no">NO</th>
+             <th className="team">TEAM</th>
+             <th>HOuSE</th>
+             <th>POINTS</th>
+          </tr>
+          {OG_row(data)}
+          
+        </table>
+    </div>
+
+  </div>
+  
+  );
+}
+
+function OG_row(data){
+  return(
+  <>
+  {
+    data.map((value, index) => (
+      <tr>
+        <td>{index+1}</td>
+        <td>{value.og}</td>
+        <td>{value.house}</td>
+        <td>{value.points} </td>
+      </tr>
+    ))
+  }
+  </>
+  )
 }
--- src/component/leaderboard_manager/index.js
+++ src/component/leaderboard_manager/index.js
@@ -1,60 +1,8 @@
 import React, { Component } from "react";
-import "./lb_manager.css";
-import Title from './Group.png';
-import axios from "axios";
-import { render } from "@testing-library/react";
 
 export default function LBManager(props) {
-  const [data, setData] = React.useState([]);
-  const getData = async() => {
-    const res = await fetch("http://gtd-xxiv-website-backend.herokuapp.com/")
-    const og_data = await res.json()
-    setData(og_data) 
-  }
-
-  React.useEffect(() => {getData()}, []);
-
-  var hasVScroll = document.body.scrollHeight > document.body.clientHeight;
-
-  return (
-  <div className="LB_manager">
-
-    <img src={Title} className="title"/>
-
-    <div className="leaderboard_parent">
-        <table className="leaderboard_child">
-          <tr>
-             <th className="no">NO</th>
-             <th className="team">TEAM</th>
-             <th>HOuSE</th>
-             <th>POINTS</th>
-          </tr>
-          {OG_row(data)}
-
-          {console.log(hasVScroll)}
-          
-        </table>
-    </div>
-
-  </div>
-  
-  );
+  return <div>LBManager</div>;
 }
 
-function OG_row(data){
-  return(
-  <>
-  {
-    data.map((value, index) => (
-      <tr>
-        <td>{index+1}</td>
-        <td>{value.og}</td>
-        <td>{value.house}</td>
-        <td>{value.points} </td>
-      </tr>
-    ))
-  }
-  </>
-  )
-}
+
 
