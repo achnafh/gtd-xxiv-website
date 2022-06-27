3fac208cd5ef6733252298f531f0c6e2b5a10c61
Aftanza <zazaprabowo@gmail.com>
2022-06-27 18:17:43 +0800

    [FAQ] added triangle

--- src/component/faq/MyCollapse.js
+++ src/component/faq/MyCollapse.js
@@ -1,20 +1,26 @@
 import {Button, Collapse} from "react-bootstrap";
+import triangle from "./img/triangle.png";
 
 const CollapseOne = ({open, setOpen, buttonText, collapseText}) => {
     return (
         <>
-            <div className="mb-2 text-center">
-                <Button
-                    variant="primary"
-                    size="lg"
-                    onClick={() => setOpen(!open)}
-                    aria-controls="collapse-text-one"
-                    aria-expanded={open}
+            <div className="collapse-container">
+                <div className="mb-2 text-center">
+                    <Button
+                        variant="primary"
+                        size="lg"
+                        onClick={() => setOpen(!open)}
+                        aria-controls="collapse-text-one"
+                        aria-expanded={open}
 
-                >
-                    {buttonText}
-                </Button>
+                    >
+                        {buttonText}
+
+                        <img src={triangle} alt=""/>
+                    </Button>
+                </div>
             </div>
+
 
             <Collapse in={open}>
                 <div id="collapse-text-one" className="collapse-text-general">
