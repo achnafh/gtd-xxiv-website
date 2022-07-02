0f3cd0f6c960161779f51ce8200b8cf0f5043418
GeeAnLim <gianaristolim@gmail.com>
2022-07-03 01:40:03 +0700

    navbar logo

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,5 +1,7 @@
 import React from "react";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import NavigationBar from "./NavigationBar";
 
 export default function Navbar(props) {
-  return <div>Navbar</div>;
+  return <NavigationBar />;
 }
