be6056f5afed5fe61ffd4078b8799fb710f62ff2
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-07-02 22:28:19 +0700

    [About Us] added changes according to the right content

--- src/component/about/events-content.js
+++ src/component/about/events-content.js
@@ -1,28 +1,36 @@
 import GTD1 from "./image/GTD 1.png";
 import GTD2 from "./image/GTD 2.png";
 import GTD3 from "./image/GTD 3.png";
+import GTD0 from "./image/logo.png";
 
 const content = [
   {
-    id: 1,
+    id: 0,
     title: "PAST EVENTS",
+    image: GTD0,
+    flip: "This balance of power is carefully maintained by Cronos, Father of the old Gods, with the power of the all-powerful, Pandora's Box. But everything changed when Cronos was murdered. Infighting and civil wars began to sprung up across the Earth as the Gods struggle to gain supremacy over each other. The Pandora's Box wields the power to end all wars between the realms, and we need every bit of help we can get to acquire the box.",
+    name: "GTD XXIII: RESURGENCE",
+  },
+  {
+    id: 1,
+    title: "",
     image: GTD1,
-    flip: "After the seemingly everlasting territorial disputes and manslaughters came to an end, the Godfather subdivided the newly established nation into four specialized semi-autonomous districts each ruled by a district leader chosen from those of the upper echelons. Alas, it seems that recently, our nation has yet been plagued with turmoil. It all began when a series of misfortunes struck each of the four districts, at too perfect timing for us to regard them as coincidental. Recruits, we need you to safeguard our hard-earned peace. . ..",
+    flip: "Fun-filled weekend with seniors from NTU and a chance to get to know other freshies! It is also the first GTD to be done under covid regulations",
     name: "GTD XXII: INSURGIO",
   },
   {
     id: 2,
     title: "",
     image: GTD2,
-    flip: "After the seemingly everlasting territorial disputes and manslaughters came to an end, the Godfather subdivided the newly established nation into four specialized semi-autonomous districts each ruled by a district leader chosen from those of the upper echelons. Alas, it seems that recently, our nation has yet been plagued with turmoil. It all began when a series of misfortunes struck each of the four districts, at too perfect timing for us to regard them as coincidental. Recruits, we need you to safeguard our hard-earned peace. . ..",
+    flip: "After the seemingly everlasting territorial disputes and manslaughters came to an end, the Godfather subdivided the newly established nation into four specialized semi-autonomous districts each ruled by a district leader chosen from those of the upper echelons. Alas, it seems that recently, our nation has yet been plagued with turmoil. It all began when a series of misfortunes struck each of the four districts, at too perfect timing for us to regard them as coincidental. Recruits, we need you to safeguard our hard-earned peace. . .",
     name: "GTD XXI: INXXIGNIA",
   },
   {
     id: 3,
     title: "",
     image: GTD3,
-    flip: "After the seemingly everlasting territorial disputes and manslaughters came to an end, the Godfather subdivided the newly established nation into four specialized semi-autonomous districts each ruled by a district leader chosen from those of the upper echelons. Alas, it seems that recently, our nation has yet been plagued with turmoil. It all began when a series of misfortunes struck each of the four districts, at too perfect timing for us to regard them as coincidental. Recruits, we need you to safeguard our hard-earned peace. . ..",
-    name: "GTD XXII: EXODIA",
+    flip: "Exodia is a paradise. A prosperous and peaceful land for centuries, where the four legendary weapons (Hammer, Daggers, Shield and Arrow) reside. These weapons originally belonged to four legendary warriors, who established the golden age of this kingdom. However, this is not everlasting. The slightest crack in trust, threatens the peace of this ever so wonderful kingdom. Can Exodia bring back the beauty it once held before?",
+    name: "GTD XX: EXODIA",
   },
 ];
 export default content;
--- src/component/about/sponsor.js
+++ src/component/about/sponsor.js
@@ -6,8 +6,15 @@ export default function Sponsor(props) {
     <div className="general">
       <div className="sponsor">
         <h2>CHECK OUT OUR SPONSOR</h2>
-        <div className="sponsor-image">
-          <img src={sponsor} alt="" style={{ height: "100%", width: "100%" }} />
+        <div class="fullwrap">
+          <img src={sponsor} alt="" className="sponsor-image-img" />
+          <a href="https://www.tanotofoundation.org/" target="_blank">
+            <div class="fullcap">
+              <p class="img__description" style={{ fontSize: 30 }}>
+                Check me out!
+              </p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
