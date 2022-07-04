f2cb31b2ad1ca6e1b1326cb71f2223da5ec6d138
GeeAnLim <gianaristolim@gmail.com>
2022-07-04 14:04:26 +0700

    drawer updated

--- src/component/navbar/DrawerComp.js
+++ src/component/navbar/DrawerComp.js
@@ -6,12 +6,29 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Tabs,
+  Tab,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-const pages = ["Products", "Services", "ABoutUs", "ContactUs"];
+import { Link } from "react-router-dom";
+
+const pages = [
+  "HOME",
+  "ABOUT US",
+  "OUR COMMITTEE",
+  "EVENTS",
+  "FAQ",
+  "LEADERBOARD",
+];
+
 const DrawerComp = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
+  const [value, setValue] = useState();
 
+  //function to close the drawer when the tab is chosen
+  const onClickTab = () => {
+    setOpenDrawer(false);
+  };
   return (
     <React.Fragment>
       <Drawer
@@ -19,7 +36,7 @@ const DrawerComp = () => {
         open={openDrawer}
         onClose={() => setOpenDrawer(false)}
       >
-        <List>
+        {/* <List>
           {pages.map((page, index) => (
             <ListItemButton key={index}>
               <ListItemIcon>
@@ -27,7 +44,42 @@ const DrawerComp = () => {
               </ListItemIcon>
             </ListItemButton>
           ))}
-        </List>
+        </List> */}
+        <Tabs
+          orientation="vertical"
+          sx={{ marginLeft: "auto" }}
+          TabIndicatorProps={{ style: { background: "white" } }}
+          textColor="inherit"
+          value={value}
+          onChange={(e, value) => setValue(value)}
+        >
+          <Tab onClick={onClickTab} label="HOME" component={Link} to={"/"} />
+          <Tab
+            onClick={onClickTab}
+            label="ABOUT US"
+            component={Link}
+            to={"/about"}
+          />
+          <Tab
+            onClick={onClickTab}
+            label="OUR COMMITTEE"
+            component={Link}
+            to={"/committee"}
+          />
+          <Tab
+            onClick={onClickTab}
+            label="EVENTS"
+            component={Link}
+            to={"/event"}
+          />
+          <Tab onClick={onClickTab} label="FAQ" component={Link} to={"/faq"} />
+          <Tab
+            onClick={onClickTab}
+            label="LEADERBOARD"
+            component={Link}
+            to={"/leaderboard"}
+          />
+        </Tabs>
       </Drawer>
       <IconButton
         sx={{ color: "white", marginLeft: "auto" }}
