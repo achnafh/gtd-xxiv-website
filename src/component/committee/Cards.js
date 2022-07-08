5946ae68f0b5a723b639eeaf217375141cccceba
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-07-08 21:05:38 +0700

    [Bugs] fixed some css bugs

--- src/component/committee/Cards.js
+++ src/component/committee/Cards.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 import "./Main.css";
 import "./Cards.css";
-import profileData from './Database';
+import profileData from "./Database";
 
+const Cards = ({ position, slideIndex }) => {
+  const cardBG = "/images/committee/profile-background.png";
+  const checkMaincomm = (position) => position.startsWith("m");
+  console.log(slideIndex);
 
-const Cards = ({position, slideIndex}) => {
-    const cardBG = "/images/committee/profile-background.png";
-    const checkMaincomm = (position) => position.startsWith("m");
-    console.log(slideIndex)
+  return (
+    <div
+      className={`${
+        checkMaincomm(position) ? "maincomm-container" : "subcomm-container"
+      }`}
+    >
+      <section className={`${position} active`}>
+        {profileData[slideIndex - 1][`${position}`].map((person) => (
+          <div className="card">
+            <img src={cardBG} alt="" />
+            <div className="overlay-card"></div>
+            <div className="photo">
+              <img src={person.photo} alt="" />
+            </div>
 
-    return ( 
-        <div className={`${checkMaincomm(position) ? "maincomm-container" : "subcomm-container"}`}>
-                <section className={`${position} active`}>
-                    {profileData[slideIndex-1][`${position}`].map((person)=>(
-                        <div className="card">
-                            <img src={cardBG} alt="" />
-                            <div className="overlay"></div>
-                                <div className="photo">
-                                    <img src={person.photo} alt="" />
-                                </div>
+            <div className="content">
+              <h2>{person.name}</h2>
+              <p>{person.major}</p>
+              <p>Position</p>
+              <p>{`${profileData[slideIndex - 1].division}`}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+      ))
+    </div>
+  );
+};
 
-                                <div className="content">
-                                    <h2>{person.name}</h2>
-                                    <p>{person.major}</p>
-                                    <p>Position</p>
-                                    <p>{`${profileData[slideIndex-1].division}`}</p>
-                                </div>
-                        </div>
-                    ))}
-                </section>
-                ))
-        </div>
-     );
-}
- 
 export default Cards;
