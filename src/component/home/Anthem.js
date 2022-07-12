c2221f0d2b1d55808f1ac5117ea767ac741fa8db
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-07-12 14:45:29 +0800

    [Home] added soundcloud

--- src/component/home/Anthem.js
+++ src/component/home/Anthem.js
@@ -8,15 +8,34 @@ export default function Anthem() {
         <img src={title} className="home_anthem_title_img"></img>
       </div>
       <div className="home_anthem_container">
+        <p className="home_text">Verse</p>
         <p className="home_text">
-          From different places, different times With a common goal in our mind
-          We want to enjoy this moment while it last Have yo ever stopped and
-          wonder why That now we can share a common sk So many of us with
-          different childhoods Different Dreams, different lives I know now what
-          the answer i I'm sure I'm gonna mis This special day when we get
-          togethe As cloud drift and seasons flee We will porbably not meet And
-          different lives we'll live No once we'll forget this great day we have
+          From different places, different times <br />
+          With a common goal in our minds <br />
+          We want to enjoy this moment while it lasts <br />
+          Have you ever stopped and wonder why <br />
+          That now we can share a common sky <br />
+          So many of us with different childhoods <br />
+          Different dreams, different lives <br />
         </p>
+        <p className="home_text">Chorus</p>
+        <p className="home_text">
+          I know now what the answer is <br />
+          I'm sure I'm gonna miss <br />
+          This special day when we get together <br />
+          As clouds drift and seasons flee <br />
+          We will porbably not meet <br />
+          And different lives we'll lives <br />
+          No once we'll forget this great day we have <br />
+        </p>
+        <iframe
+          width="80%"
+          height="166"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/455672094&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        ></iframe>
       </div>
     </div>
   );
