4010d7e6e35cd4eb38508eb62ab84044f4f586ca
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-07-16 20:57:52 +0800

    [Home] added content according to each div

--- src/component/home/Logos.js
+++ src/component/home/Logos.js
@@ -17,7 +17,16 @@ export default function Logos() {
           <img src={neonBlue} className="house_img" />
           <div className="home_house_per_hover">
             <div className="home_house_per_hover_content">
-              <p className="home_text">Harley Quinn</p>
+              <p className="home_text">
+                Harley Quinn
+                <br />A former psychiatric intern who was fascinated in learning
+                the psychology of asylum people. After having an abusive affair
+                with a manipulative inmate, Harley Quinn were left in
+                unpredictable mental mood swings. Many people underestimated her
+                after looking at her abnormal behaviour, but Kingpin sees her as
+                a great addition to the arsenal as she grew more violent than
+                ever
+              </p>
             </div>
           </div>
         </div>
@@ -29,7 +38,17 @@ export default function Logos() {
           <img src={neonYellow} className="house_img" />
           <div className="home_house_per_hover">
             <div className="home_house_per_hover_content">
-              <p className="home_text">Reversed Flash</p>
+              <p className="home_text">
+                Reversed Flash
+                <br />
+                an ex-professor who turned into a criminal after a bitter
+                rejection from the scientific community regarding his claims
+                that he can recreate the formula used by The Flash to obtain his
+                super speed. Despite this, the formula he recreated was
+                temporary, and he started to seek help from other criminals and
+                encountered Kingpin. After promising vengeance, Kingpin hired
+                him and uses the formula to bolster his forces
+              </p>
             </div>
           </div>
         </div>
@@ -43,7 +62,15 @@ export default function Logos() {
           <img src={neonGreen} className="house_img" />
           <div className="home_house_per_hover">
             <div className="home_house_per_hover_content">
-              <p className="home_text">Poison Ivy</p>
+              <p className="home_text">
+                Poison Ivy
+                <br />A former PhD graduate and was once a world-renowned
+                botanist. However, her dark past left her being an intoxicated
+                victim of a brutal experiment conducted by one of her close
+                acquaintances, which made her able to control all living plants.
+                Capitalizing on her rage of betrayal, Kingpin adopted her to the
+                team and has since become a force of nature
+              </p>
             </div>
           </div>
         </div>
@@ -55,7 +82,16 @@ export default function Logos() {
           <img src={neonRed} className="house_img" />
           <div className="home_house_per_hover">
             <div className="home_house_per_hover_content">
-              <p className="home_text">Peacemaker</p>
+              <p className="home_text">
+                Peacemaker
+                <br />
+                the embodiment of peak human physical conditions. When the world
+                turned rogue, he started to lose his sense of justice and
+                decided to join Kingpin in his conquest to rule the underworld.
+                Equipped with super strength, state-of-the-art weaponry, and
+                lethal marksmanship, all enemies trembled before him as he can
+                subdue each of them with ease.
+              </p>
             </div>
           </div>
         </div>
--- src/component/home/Storyline.js
+++ src/component/home/Storyline.js
@@ -9,13 +9,13 @@ export default function Storyline(props) {
       </div>
       <div className="home_story_container" data-aos="fade-left">
         <p className="home_text">
-          From different places, different times With a common goal in our mind
-          We want to enjoy this moment while it last Have yo ever stopped and
-          wonder why That now we can share a common sk So many of us with
-          different childhoods Different Dreams, different lives I know now what
-          the answer i I'm sure I'm gonna mis This special day when we get
-          togethe As cloud drift and seasons flee We will porbably not meet And
-          different lives we'll live No once we'll forget this great day we have
+          After subduing the entire criminal underworld, Kingpin ruled with an
+          iron fist and has been notoriously known for his violent reign.
+          However, as his time is coming to an end, he held an open contest for
+          his fellow companions to determine the next ruler of the underworld.
+          <br />
+          <br />
+          Or at least, that's what he wants us to believe...
         </p>
       </div>
     </div>
