92aa3d7fe5a93720ac9208a99796a292b5585cd2
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-07-19 01:00:59 +0800

    [Event] added schedule

--- src/component/event/Header.js
+++ src/component/event/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./event.css";
 import image from "./image/Group (1).png";
+import schedule from "./schedule.pdf";
 export default function Header(props) {
   return (
-    <div className="bigimage" data-aos="zoom-out">
-      <div className="overlay">
-        <img className="text" src={image} alt="" />
-        <h2>
-          <span>GTD XXIV</span>
-        </h2>
+    <>
+      <div className="bigimage" data-aos="zoom-out">
+        <div className="overlay">
+          <img className="text" src={image} alt="" />
+          <h2>
+            <span>GTD XXIV</span>
+          </h2>
+        </div>
       </div>
-    </div>
+      <div className="event-text-div">
+        <a className="event-text" href={schedule} target="_blank">
+          click here to see schedule!
+        </a>
+      </div>
+    </>
   );
 }
